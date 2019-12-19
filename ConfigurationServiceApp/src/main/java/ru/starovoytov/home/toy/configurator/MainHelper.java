752a1dd84017b8ef28ab41de6da05a33af3b4617
StarovoytovProg <adminofusa@gmail.com>
2019-12-19 09:36:12 +0300

    Инициализация сервиса конфигурации.

---
+++ ConfigurationServiceApp/src/main/java/ru/starovoytov/home/toy/configurator/MainHelper.java
@@ -0,0 +1,13 @@
+package ru.starovoytov.home.toy.configurator;
+
+/**
+ * Стартовый класс сервиса конфигурации
+ *
+ * @author starovoytov
+ * @since 2019.12.19
+ */
+public class MainHelper {
+	public static void main(String[] args) {
+		System.out.println("I'm alive!");
+	}
+}
