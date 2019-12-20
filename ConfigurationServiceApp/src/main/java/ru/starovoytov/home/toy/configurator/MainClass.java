bf48ef5df4bdf965d17dbe259e0031014c0eedc5
StarovoytovProg <adminofusa@gmail.com>
2019-12-20 21:13:01 +0300

    Старт сервиса.

---
+++ ConfigurationServiceApp/src/main/java/ru/starovoytov/home/toy/configurator/MainClass.java
@@ -0,0 +1,43 @@
+package ru.starovoytov.home.toy.configurator;
+
+import ru.starovoytov.home.toy.web.library.UndertowHttpService;
+
+import java.util.ArrayList;
+
+/**
+ * Стартовый класс сервиса конфигурации
+ *
+ * @author starovoytov
+ * @since 2019.12.19
+ */
+public final class MainClass {
+
+	/**
+	 * Инстанс сервиса
+	 */
+	private final transient UndertowHttpService service;
+
+	/**
+	 * Конструктор по умолчанию
+	 */
+	private MainClass() {
+		service = new UndertowHttpService(1111, "localhost", new ArrayList<>());
+	}
+
+	/**
+	 * Запуск сервиса
+	 */
+	public void start() {
+		service.start();
+	}
+
+	/**
+	 * Точка запуска
+	 *
+	 * @param args аргументы командной строки
+	 */
+	public static void main(final String[] args) {
+		final MainClass mainClass = new MainClass();
+		mainClass.start();
+	}
+}
