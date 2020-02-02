c796948b66f28abdede09fca0783c695879b80a3
StarovoytovProg <adminofusa@gmail.com>
2020-02-02 21:03:20 +0300

    Загрузка параметров хост и порт из конфигуратора.

--- ConfigurationServiceApp/src/main/java/ru/starovoytov/home/toy/configurator/Application.java
+++ ConfigurationServiceApp/src/main/java/ru/starovoytov/home/toy/configurator/Application.java
@@ -3,6 +3,10 @@ package ru.starovoytov.home.toy.configurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import ru.starovoytov.home.toy.configurator.configuration.Configurator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Стартовый класс сервиса конфигурации
@@ -20,6 +24,11 @@ public class Application {
 	 * @param args аргументы
 	 */
 	public static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
+		final SpringApplication app = new SpringApplication(Application.class);
+		final Map<String, Object> properties = new HashMap<>();
+		properties.put("server.port", Configurator.getInstance().getPort());
+		properties.put("server.host", Configurator.getInstance().getHost());
+		app.setDefaultProperties(properties);
+		app.run(args);
 	}
 }
