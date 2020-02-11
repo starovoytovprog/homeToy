0fb68d72ebc725f86acab7dcd6e947faa6997b9b
StarovoytovProg <adminofusa@gmail.com>
2020-02-11 10:49:41 +0300

    Тест получения значения из конфигуратора.

--- CommonLib/src/test/java/ru/starovoytov/home/toy/common/libs/configuration/ConfigurationServiceHelperTest.java
+++ CommonLib/src/test/java/ru/starovoytov/home/toy/common/libs/configuration/ConfigurationServiceHelperTest.java
@@ -9,6 +9,7 @@ import ru.starovoytov.home.toy.test.utils.undertow.UndertowHttpService;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static ru.starovoytov.home.toy.common.libs.configuration.ConfigurationServiceHelper.getValueFromService;
 import static ru.starovoytov.home.toy.test.utils.TestUtils.getFreePort;
@@ -30,14 +31,36 @@ class ConfigurationServiceHelperTest {
 	@BeforeAll
 	public static void startService() {
 		final List<HttpHandlerDescriptor> descriptors = new ArrayList<>();
+		descriptors.add(new HttpHandlerDescriptor("conf", new ConfiguratorHandler()));
 		service = new UndertowHttpService(SERVICE_PORT, "localhost", descriptors);
 		service.start();
 	}
 
+	/**
+	 * Тест получения значения из сервиса по несуществующему адресу
+	 */
 	@Test
 	public void testGetFromBadAddress() {
 		final String value = getValueFromService("key", "bad address");
 		assertNull(value, "Not null value from bad address");
+	}
+
+	/**
+	 * Тест получения значения из сервиса
+	 */
+	@Test
+	public void testGetFromConfigurator() {
+		final String value = getValueFromService("key", "http://localhost:" + SERVICE_PORT + "/conf");
+		assertEquals("[key] value", value, "Bad value");
+	}
+
+	/**
+	 * Тест получения значения из сервиса 2
+	 */
+	@Test
+	public void testGetFromConfigurator2() {
+		final String value = getValueFromService("key2", "http://localhost:" + SERVICE_PORT + "/conf");
+		assertEquals("[key2] value", value, "Bad value");
 	}
 
 	/**
