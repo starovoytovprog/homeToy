bdafd4595534e0979da72f51c676f2b83bd346a3
StarovoytovProg <adminofusa@gmail.com>
2020-02-12 09:04:07 +0300

    Тест запуска сервиса

---
+++ VkCollectorApp/src/test/java/ru/starovoytov/home/toy/vk/ApplicationTest.java
@@ -0,0 +1,33 @@
+package ru.starovoytov.home.toy.vk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Тест сервиса {@link Application}
+ *
+ * @author starovoytov
+ * @since 2020.02.12
+ */
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@SuppressWarnings({"PMD.AtLeastOneConstructor", "PMD.TooManyStaticImports"})
+class ApplicationTest {
+	@Autowired
+	private transient MockMvc mvc;
+
+	@Test
+	@SuppressWarnings({"PMD.LawOfDemeter", "PMD.SignatureDeclareThrowsException"})
+	public void helloController() throws Exception {
+		final MvcResult result = mvc.perform(get("/hello")).andExpect(status().isOk()).andReturn();
+		assertTrue(result.getResponse().getContentAsString().contains("Hello!"), "Bad hello text");
+	}
+}
